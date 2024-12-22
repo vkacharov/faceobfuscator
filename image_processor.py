9496f0f19f7ab80c24eabe7e52b48936cfe4bc59
Velizar Kacharov <velizarkacharov@Velizars-MacBook-Air.local>
2024-12-22 17:27:28 +0200

    Use s3 to call rekognition

--- image_processor.py
+++ image_processor.py
@@ -2,52 +2,61 @@ from PIL import Image
 import io
 import os
 import boto3
+import base64
 from utils import load_file_path
+from s3_uploader import S3Uploader
 
 class ImageProcessor:
     rekognition_client = None
+    bear_image = None
 
     @classmethod
     def initialize(cls):
-        cls.rekognition_client = boto3.client(
-            "rekognition", 
-            aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name = os.getenv("AWS_DEFAULT_REGION")
-            )
+        cls.rekognition_client = boto3.client("rekognition")
+        cls.bear_image = Image.open(load_file_path("bear.png")).convert("RGBA")
     
     def __init__(self, image_file, output_directory):
         self.image_file = image_file
         self.output_directory = output_directory
-        self.bear_image = Image.open(load_file_path("bear.png")).convert("RGBA")
 
     def obfuscate_image(self):
-        with open(self.image_file, "rb") as image_file:
-            image_bytes = image_file.read()
-            faces = self.__detect_faces(image_bytes)
+        s3_prefix = self.__base64_encode(self.output_directory)
+        object_name = S3Uploader().upload_file(s3_prefix, self.image_file)
+        try:
+            with open(self.image_file, "rb") as image_file_content:
+                image_bytes = image_file_content.read()
+                raw_image = Image.open(io.BytesIO(image_bytes))
+                image = self.__rotate_image(raw_image)
 
-            raw_image = Image.open(io.BytesIO(image_bytes))
-            image = self.__rotate_image(raw_image)
+                faces = self.__detect_faces(object_name)
+                for face in faces:
+                    low_age_range = face["AgeRange"]["Low"]
+                    if (low_age_range <= 18):
+                        width, height, left, top = self.__calculate_face_rectangle(image.size, face)
+                        resized_bear = self.bear_image.resize((int(width), int(height)))
+                        image.paste(resized_bear, (int(left), int(top)), mask=resized_bear)
 
-            for face in faces:
-                low_age_range = face["AgeRange"]["Low"]
-                if (low_age_range <= 18):
-                    width, height, left, top = self.__calculate_face_rectangle(image.size, face)
-                    resized_bear = self.bear_image.resize((int(width), int(height)))
-                    image.paste(resized_bear, (int(left), int(top)), mask=resized_bear)
-
-            file_name = os.path.basename(self.image_file)
-            fixed_image_name = self.output_directory + "/" + self.__generate_fixed_file_name(file_name)
-            image.save(fixed_image_name)
-            return fixed_image_name
+                file_name = os.path.basename(self.image_file)
+                fixed_image_name = self.output_directory + "/" + self.__generate_fixed_file_name(file_name)
+                image.save(fixed_image_name)
+                return fixed_image_name
+        finally:
+            S3Uploader().delete_file(object_name)
 
     def __generate_fixed_file_name(self, file_name):
         name, ext = os.path.splitext(file_name)
         return f"{name}-fixed{ext}"
 
-    def __detect_faces(self, image):
+    def __detect_faces(self, object_name):
         response = ImageProcessor.rekognition_client.detect_faces(
-            Image={"Bytes" : image}, Attributes=["AGE_RANGE"]
+            Image={
+                "S3Object" : {
+                    'Bucket': 'plushenomeche-visitation-photos',
+                    'Name': object_name,
+                }
+            }, 
+            
+            Attributes=["AGE_RANGE"]
         )
         faces = [face for face in response["FaceDetails"]]
         return faces
@@ -87,3 +96,9 @@ class ImageProcessor:
             return rotated_image
         except (AttributeError, KeyError, IndexError):
             pass
+
+    def __base64_encode(self, text):
+        text_bytes = text.encode("utf-8")
+        base64_bytes = base64.urlsafe_b64encode(text_bytes)
+        base64_text = base64_bytes.decode("ascii").rstrip("=")
+        return base64_text
---
+++ s3_uploader.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+
+class S3Uploader:
+    _instance = None
+    s3_client = None
+    bucket_name = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+            cls.s3_client = boto3.client("s3")
+            cls.bucket_name = os.getenv('S3_BUCKET_NAME')
+        return cls._instance
+    
+
+    def upload_file(self, prefix, file_name):
+        object_name = f"{prefix}/{os.path.basename(file_name)}"
+        self.s3_client.upload_file(file_name, self.bucket_name, object_name)
+        return object_name
+    
+    def delete_file(self, object_name):
+        self.s3_client.delete_object(
+            Bucket=self.bucket_name,
+            Key=object_name
+        )
